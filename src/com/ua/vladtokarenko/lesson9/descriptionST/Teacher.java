0e9ede2e2c294c0cca5f157505390d5059057ec5
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-09 21:55:25 +0200

    description Teacher and Student

---
+++ src/com/ua/vladtokarenko/lesson9/descriptionST/Teacher.java
@@ -0,0 +1,11 @@
+package com.ua.vladtokarenko.lesson9.descriptionST;
+
+public class Teacher {
+    public String name;
+    public String surname;
+    public int age;
+    public String room;
+    public String Teacher() {
+        return name + " " + surname + " " + age + " " + room;
+    }
+}
