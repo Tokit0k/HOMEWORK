066cec073afe1a6955567002c29a567cbf253468
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-10 13:24:35 +0200

    dont undestend (

--- src/com/ua/vladtokarenko/lesson9/arreynumber/Main.java
+++ src/com/ua/vladtokarenko/lesson9/arreynumber/Main.java
@@ -1,21 +1,9 @@
 package com.ua.vladtokarenko.lesson9.arreynumber;
-
-import java.util.Arrays;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        int length = 10;
-        int[] arr = new int[length];
-        Random random = new Random();
-        for (int i = 1; i < length; i++) {
-            arr[i] = random.nextInt(9);
-
-        }
-        Arrays.sort(arr);
-        for (int arr1 : arr) {
-            System.out.println(arr1);
-
-        }
+        GrowthArrey gArrey = new GrowthArrey();
+        gArrey.length = 3;
+        gArrey.arr  = new int[gArrey.length];
+        System.out.println(gArrey.growArrey());
     }
 }
