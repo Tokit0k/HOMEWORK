bee4a6bd61acf4ce0f2323bd289e12b0615f879f
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-10 16:13:29 +0200

    dont undestend (

--- src/com/ua/vladtokarenko/lesson9/arreynumber/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ua.vladtokarenko.lesson9.arreynumber;
-public class Main {
-    public static void main(String[] args) {
-        GrowthArrey gArrey = new GrowthArrey();
-        gArrey.length = 3;
-        gArrey.arr  = new int[gArrey.length];
-        System.out.println(gArrey.growArrey());
-    }
-}
