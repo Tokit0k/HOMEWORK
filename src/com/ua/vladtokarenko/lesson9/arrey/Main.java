1d67ff41e36d50de8e78b72dfcbbe0dac22a2593
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-12 19:22:39 +0200

    Home Work done:)

--- src/com/ua/vladtokarenko/lesson9/arrey/Main.java
+++ src/com/ua/vladtokarenko/lesson9/arrey/Main.java
@@ -2,9 +2,9 @@ package com.ua.vladtokarenko.lesson9.arrey;
 
 public class Main {
     public static void main(String[] args) {
-        Arreys arrey = new Arreys(10);
+        Arreys arrey = new Arreys(9);
         System.out.println(arrey.length);
         System.out.println(arrey.ArreysSortDescendingm());
-
+        System.out.println(arrey.ArreysSortAscendingm());
     }
 }
