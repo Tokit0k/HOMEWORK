ed4ebb7c9f9b90fbab5e05743286bd51cafffd4b
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-13 16:08:31 +0200

    done)

--- src/com/ua/vladtokarenko/homework/Array.java
+++ src/com/ua/vladtokarenko/homework/Array.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 public class Array {
     int i;
-    public Array(int length) {
+    int length;
+    int[] arr;
+
+    public void arrRandom(int length) {
         int[] arr = new int[length];
         Random random = new Random();
         for (i = 0; i < length; i++) {
@@ -12,6 +15,9 @@ public class Array {
             System.out.print(arr[i] + " ");
         }
         System.out.println(" ");
+    }
+
+    public void arrSortMin(int length) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = arr.length - 1; j > i; j--) {
                 if (arr[j - 1] < arr[j]) {
@@ -20,9 +26,11 @@ public class Array {
                     arr[j] = tmp;
                 }
             }
-            System.out.print(arr[i] + " ");
         }
-        System.out.println(" ");
+        System.out.print(arr[i] + " ");
+    }
+
+    public void arrSortMax(int length) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = arr.length - 1; j > i; j--) {
                 if (arr[j - 1] > arr[j]) {
@@ -31,7 +39,8 @@ public class Array {
                     arr[j] = tmp;
                 }
             }
-            System.out.print(arr[i] + " ");
         }
+        System.out.print(arr[i] + " ");
     }
 }
+
