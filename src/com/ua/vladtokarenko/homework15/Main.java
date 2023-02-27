d94f42c5f1296d5050b995120ef02664960f870e
Vladislav Tokarenko <vladtokarenkooo@gmail.com>
2023-02-27 23:24:14 +0200

    done + added first and last name

--- src/com/ua/vladtokarenko/homework15/Main.java
+++ src/com/ua/vladtokarenko/homework15/Main.java
@@ -2,9 +2,9 @@ package com.ua.vladtokarenko.homework15;
 
 public class Main {
     public static void main(String[] args) {
-        Flying person1 = new Pilot();
+        Flying person1 = new Pilot("Peter", "Parker");
         person1.fly();
-        Driving person2 = new Driver();
+        Driving person2 = new Driver("Ivan", "Ivanov");
         person2.drive();
     }
 }
--- src/com/ua/vladtokarenko/homework15/Pilot.java
+++ src/com/ua/vladtokarenko/homework15/Pilot.java
@@ -1,9 +1,34 @@
 package com.ua.vladtokarenko.homework15;
 
 public class Pilot extends Persona implements Flying {
+    String firstName;
+    String lastName;
+
+    public Pilot(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
     @Override
     public void fly() {
-        System.out.println("I'm Pilot");
+        System.out.println(getFirstName() + " " + getLastName() + " " + "I'm Pilot");
     }
 
 }
