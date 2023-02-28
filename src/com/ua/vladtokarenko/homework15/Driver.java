156fbed4503bf8fa63ca1c48bdcbdb89ff4847bd
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-02-28 12:46:52 +0200

    done + added first and last name

--- src/com/ua/vladtokarenko/homework15/Driver.java
+++ src/com/ua/vladtokarenko/homework15/Driver.java
@@ -1,32 +1,12 @@
 package com.ua.vladtokarenko.homework15;
 
 public class Driver extends Persona implements Driving {
-    String firstName;
-    String lastName;
-
     public Driver(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+        super(firstName, lastName);
     }
 
     @Override
     public void drive() {
-        System.out.println(getFirstName() + " " + getLastName() + " " + "i'm Driver");
+        System.out.println(getFirstName() + " " + getLastName() + " " + "I'm driving");
     }
 }
