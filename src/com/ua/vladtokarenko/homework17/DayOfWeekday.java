31339ebabcb59e08c76f36585c6ba91e6dac8ca7
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-06 23:19:00 +0200

    done (SwitchCase)

--- src/com/ua/vladtokarenko/homework17/DayOfWeekday.java
+++ src/com/ua/vladtokarenko/homework17/DayOfWeekday.java
@@ -1,29 +1,17 @@
 package com.ua.vladtokarenko.homework17;
 
 public enum DayOfWeekday {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
      static DayOfWeekday numberDays(int number) {
-        switch (number) {
-            case 1:
-                return MONDAY;
-            case 2:
-                return TUESDAY;
-            case 3:
-                return WEDNESDAY;
-            case 4:
-                return THURSDAY;
-            case 5:
-                return FRIDAY;
-            case 6:
-                return SATURDAY;
-            default:
-                return null;
-        }
+         return switch (number) {
+             case 1 -> MONDAY;
+             case 2 -> TUESDAY;
+             case 3 -> WEDNESDAY;
+             case 4 -> THURSDAY;
+             case 5 -> FRIDAY;
+             case 6 -> SATURDAY;
+             case 7 -> SUNDAY;
+             default -> null;
+         };
     }
 }
--- src/com/ua/vladtokarenko/homework17/Main.java
+++ src/com/ua/vladtokarenko/homework17/Main.java
@@ -6,5 +6,4 @@ public class Main {
         System.out.println(DayOfWeekday.numberDays(2));
         System.out.println(DayOfWeekday.numberDays(9));
     }
-
 }
