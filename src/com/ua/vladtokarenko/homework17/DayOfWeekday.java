5f9a7b2719d0d72447184c70722b89e477238484
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-06 21:35:26 +0200

    done (the second way)

--- src/com/ua/vladtokarenko/homework17/DayOfWeekday.java
+++ src/com/ua/vladtokarenko/homework17/DayOfWeekday.java
@@ -1,29 +1,32 @@
 package com.ua.vladtokarenko.homework17;
 
 public enum DayOfWeekday {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
-    public static String numberDays(int numberDays) {
-        switch (numberDays) {
-            case 1:
-                return "Monday";
-            case 2:
-                return "Tuesday";
-            case 3:
-                return "Wednesday";
-            case 4:
-                return "Thursday";
-            case 5:
-                return "Friday";
-            case 6:
-                return "Saturday";
-            default:
-                return "Більше намає днів";
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+    int numberDays;
+
+    DayOfWeekday(int numberDays) {
+        this.numberDays = numberDays;
+    }
+
+    public int getNumberDays() {
+        return numberDays;
+    }
+
+    public void setNumberDays(int numberDays) {
+        this.numberDays = numberDays;
+    }
+    static DayOfWeekday getWeekday(int dayNumber) {
+        for (DayOfWeekday weekday : DayOfWeekday.values()) {
+            if (weekday.getNumberDays() == dayNumber) {
+                return weekday;
+            }
         }
+        return null;
     }
 }
--- src/com/ua/vladtokarenko/homework17/Main.java
+++ src/com/ua/vladtokarenko/homework17/Main.java
@@ -2,9 +2,9 @@ package com.ua.vladtokarenko.homework17;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DayOfWeekday.numberDays(1));
-        System.out.println(DayOfWeekday.numberDays(2));
-        System.out.println(DayOfWeekday.numberDays(9));
+
+        System.out.println(DayOfWeekday.getWeekday(1));
+        System.out.println(DayOfWeekday.getWeekday(2));
     }
 
 }
