da49a6fdde5b451b93b238224a601be440d5919c
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-13 21:11:38 +0200

    done the method for calling 2,000,000 items does not open in the console

---
+++ src/com/ua/vladtokarenko/homework18/Student.java
@@ -0,0 +1,52 @@
+package com.ua.vladtokarenko.homework18;
+
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+    private String group;
+
+    public Student(String name, String surname, int age, String group) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return " Student " + name + " " + surname +" Age " + " " + age + " " +"Group "+ group;
+    }
+}
