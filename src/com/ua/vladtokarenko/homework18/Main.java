b0371166898995624e318718aa440d59a2f69e8f
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-16 17:30:48 +0200

    done

--- src/com/ua/vladtokarenko/homework18/Main.java
+++ src/com/ua/vladtokarenko/homework18/Main.java
@@ -8,22 +8,12 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
         for (int i = 1; i < 11; i++) {
-        list.add(i);
+            list.add(i);
         }
-        System.out.println(list);
-        System.out.println(" ");
-        System.out.println(list.get(4));
-        System.out.println(list.size());
-        System.out.println(" ");
-        List<Integer> list2 = new LinkedList<>();
-        for (int i = 0; i < 2000000; i++) {
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 1; i < 2000001; i++) {
             list2.add(i);
         }
-        list2.remove(44);
-        list2.add(1,122);
-        System.out.println(list2.get(1));
-        System.out.println(list2.size());
-        System.out.println("");
         List<Student> students = new ArrayList<>();
         students.add(new Student("Ivan", "Ivanov", 20, "A"));
         students.add(new Student("Petr", "Petrov", 21, "B"));
@@ -32,6 +22,8 @@ public class Main {
             System.out.println(student);
 
         }
+        System.out.println(list);
+        System.out.println(list2.size());
 
     }
 
