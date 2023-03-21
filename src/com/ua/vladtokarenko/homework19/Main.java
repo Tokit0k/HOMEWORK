a736f6ab0fe3c01cc3081321539754e09d70c997
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-21 16:50:56 +0200

    done

--- src/com/ua/vladtokarenko/homework19/Main.java
+++ src/com/ua/vladtokarenko/homework19/Main.java
@@ -1,38 +1,23 @@
 package com.ua.vladtokarenko.homework19;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Створюємо мапу
-        HashMap<String, String[]> map = new HashMap<>();
+        Random random = new Random();
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
 
-        // Додаємо слова та їх переклади
-        map.put("кіт", new String[]{"cat", "猫", "Katze"});
-        map.put("сонце", new String[]{"sun", "太陽", "Sonne"});
-        map.put("дерево", new String[]{"tree", "木", "Baum"});
-        map.put("вода", new String[]{"water", "水", "Wasser"});
-        map.put("книга", new String[]{"book", "本", "Buch"});
-        map.put("батько", new String[]{"father", "父", "Vater"});
-        map.put("мати", new String[]{"mother", "母", "Mutter"});
-        map.put("дім", new String[]{"house", "家", "Haus"});
-        map.put("автомобіль", new String[]{"car", "車", "Auto"});
-        map.put("програмування", new String[]{"programming", "プログラミング", "Programmierung"});
-
-        // Зчитуємо користувацький ввід
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введіть слово: ");
-        String word = scanner.nextLine();
-
-        // Отримуємо переклади з мапи та виводимо їх на екран
-        String[] translations = map.get(word);
-        if (translations != null) {
-            System.out.println("Англійська: " + translations[0]);
-            System.out.println("Японська: " + translations[1]);
-            System.out.println("Німецька: " + translations[2]);
-        } else {
-            System.out.println("Переклад для цього слова не знайдено");
+        for (int i = 0; i < 1000; i++) {
+            int randomNumber = random.nextInt(50) + 1;
+            hashSet.add(randomNumber);
+            treeSet.add(randomNumber);
+            linkedHashSet.add(randomNumber);
         }
+
+        System.out.println("HashSet: " + hashSet);
+        System.out.println("TreeSet: " + treeSet);
+        System.out.println("LinkedHashSet: " + linkedHashSet);
     }
 }
