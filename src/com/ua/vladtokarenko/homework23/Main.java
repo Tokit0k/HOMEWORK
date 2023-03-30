be903f7d73d64b8171e46b9c1e345f3309fa63c6
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-31 01:05:33 +0300

    done

--- src/com/ua/vladtokarenko/homework23/Main.java
+++ src/com/ua/vladtokarenko/homework23/Main.java
@@ -14,9 +14,11 @@ public class Main {
                 .collect(Collectors.groupingBy(surname -> surname, Collectors.counting()));
         System.out.println("Кількість повторень прізвищ:");
         System.out.println(surnameCountMap);
+        System.out.println("");
+
         List<Integer> numbers = new Random().ints(10, 0, 21).boxed().toList();
-        int sum = new Random().ints(20, 0, 100)
-                .sum();
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(numbers);
         System.out.println("Сума випадкових чисел: " + sum);
     }
 }
