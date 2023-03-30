ca06d3739361f7cfa0b0e85b014dea95519cea6c
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-03-30 16:11:01 +0300

    done

--- src/com/ua/vladtokarenko/homework21/Main.java
+++ src/com/ua/vladtokarenko/homework21/Main.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Tokarenko", 5.0));
-        students.add(new Student("Vlasov", 4.5));
-        students.add(new Student("Ankura", 2.5));
-        students.add(new Student("Ivanov", 4.5));
-        students.add(new Student("Petrov", 1.0));
+        students.add(new Student("Tokarenko", 5.0, 18));
+        students.add(new Student("Vlasov", 4.5, 23));
+        students.add(new Student("Ankura", 2.5, 43));
+        students.add(new Student("Ivanov", 4.5, 19));
+        students.add(new Student("Petrov", 1.0, 20));
 
         System.out.println("Original list:");
         for (Student student : students) {
-            System.out.println(student.getSurname() + " " + student.getAverageGrade());
+            System.out.println(student.getSurname() + " " + student.getAverageGrade() + " " + student.getAge());
         }
 
         Comparator<Student> bySurname = Comparator.comparing(Student::getSurname);
@@ -23,7 +23,7 @@ public class Main {
         System.out.println(" ");
         System.out.println("List bySurname:");
         for (Student student : students) {
-            System.out.println(student.getSurname() + " " + student.getAverageGrade());
+            System.out.println(student.getSurname() + " " + student.getAverageGrade() + " " + student.getAge());
         }
         System.out.println(" ");
         Comparator<Student> byAverageGrade = Comparator.comparingDouble(Student::getAverageGrade);
@@ -31,7 +31,15 @@ public class Main {
 
         System.out.println("List byAverageGrade:");
         for (Student student : students) {
-            System.out.println(student.getSurname() + " " + student.getAverageGrade());
+            System.out.println(student.getSurname() + " " + student.getAverageGrade() + " " + student.getAge());
+        }
+        Comparator<Student> sortName = Comparator.comparing(Student::getAge);
+        students.sort(sortName);
+        System.out.println(" ");
+        System.out.println("sort age: ");
+        for (Student student : students) {
+            System.out.println(student.getSurname() + " " + student.getAverageGrade() + " " + student.getAge());
+
         }
     }
 
--- src/com/ua/vladtokarenko/homework21/Student.java
+++ src/com/ua/vladtokarenko/homework21/Student.java
@@ -3,15 +3,21 @@ package com.ua.vladtokarenko.homework21;
 public class Student {
     private final String surname;
     private final double averageGrade;
+    private final int age;
 
-    public Student(String surname, double averageGrade) {
+    public Student(String surname, double averageGrade, int age) {
         this.surname = surname;
         this.averageGrade = averageGrade;
+        this.age = age;
     }
     public String getSurname() {
         return surname;
     }
     public double getAverageGrade() {
         return averageGrade;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
