a35a27556d5e34c7c7363d0ec2787489c09e24fc
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-04-18 22:41:27 +0300

    done

--- src/com/ua/vladtokarenko/homework27/FileService.java
+++ src/com/ua/vladtokarenko/homework27/FileService.java
@@ -1,17 +1,36 @@
 package com.ua.vladtokarenko.homework27;
 
-import java.io.*;
-import java.util.Scanner;
+//import java.io.File;
+//import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+//import java.util.Scanner;
 
 public class FileService {
-    private static final String FILE_NAME = "data.txt";
+    static final String FILE_NAME = "data.txt";
+//     static final String FILE_NAME2 = "data2.txt";
 
-    public static void saveToFile(String data) {
+
+//    public static void saveToFile1(String data) {
+//        try {
+//            File file = new File(FILE_NAME);
+//            FileWriter writer = new FileWriter(file, true);
+//            writer.write(data.replaceAll("\n", " ") + " ");
+//            writer.close();
+//            System.out.println();
+//            System.out.println("Строка збережена в файл");
+//        } catch (IOException e) {
+//            System.out.println("ой ой ой, не можу зберегти в файл");
+//            e.printStackTrace();
+//        }
+//    }
+    static void saveToFile(String data) {
+        Path path = Path.of(FILE_NAME);
         try {
-            File file = new File(FILE_NAME);
-            FileWriter writer = new FileWriter(file, true);
-            writer.write(data.replaceAll("\n", " ") + " ");
-            writer.close();
+            data.replaceAll("\n", " ");
+            Files.write(path, data.getBytes(), StandardOpenOption.APPEND);
             System.out.println();
             System.out.println("Строка збережена в файл");
         } catch (IOException e) {
@@ -20,22 +39,35 @@ public class FileService {
         }
     }
 
+//    public static String readFromFile1() {
+//        try {
+//            File file = new File(FILE_NAME);
+//            Scanner scanner = new Scanner(file);
+//            StringBuilder data = new StringBuilder();
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                data.append(line).append("\n");
+//            }
+//            scanner.close();
+//            return data.toString();
+//        } catch (IOException e) {
+//            System.out.println("ой ой ой, не можу зберегти в файл");
+//            e.printStackTrace();
+//            return "";
+//        }
+//    }
     public static String readFromFile() {
+        Path path = Path.of(FILE_NAME);
         try {
-            File file = new File(FILE_NAME);
-            Scanner scanner = new Scanner(file);
-            StringBuilder data = new StringBuilder();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                data.append(line).append("\n");
-            }
-            scanner.close();
-            return data.toString();
+            byte[] data = Files.readAllBytes(path);
+            return new String(data);
+
         } catch (IOException e) {
             System.out.println("ой ой ой, не можу зберегти в файл");
             e.printStackTrace();
             return "";
         }
+
     }
 
 }
--- src/com/ua/vladtokarenko/homework27/Main.java
+++ src/com/ua/vladtokarenko/homework27/Main.java
@@ -1,24 +1,46 @@
 package com.ua.vladtokarenko.homework27;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+//import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
-import static com.ua.vladtokarenko.homework27.FileService.readFromFile;
-import static com.ua.vladtokarenko.homework27.FileService.saveToFile;
+import static com.ua.vladtokarenko.homework27.FileService.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Напиши текст (або 'Вийти' щоб вийти ): ");
-            String input = scanner.nextLine();
-            if (input.equals("Вийти")) {
-                break;
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(FILE_NAME2))) {
+            while (true) {
+                System.out.println("Напиши текст (або 'Вийти' щоб вийти ): ");
+                String input = scanner.nextLine();
+                if (input.equals("Вийти")) {
+                    break;
+                }
+                saveToFile(input);
+                String allData = readFromFile();
+                System.out.println("Строка в файлі ");
+                System.out.println(allData);
+
             }
-            saveToFile(input);
-            String allData = readFromFile();
-            System.out.println("Строка в файлі ");
-            System.out.println(allData);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        scanner.close();
+//        while (true) {
+//            System.out.println("Напиши текст (або 'Вийти' щоб вийти ): ");
+//            String input = scanner.nextLine();
+//            if (input.equals("Вийти")) {
+//                break;
+//            }
+//            saveToFile(input);
+//            String allData = readFromFile();
+//            System.out.println("Строка в файлі ");
+//            System.out.println(allData);
+//        }
+//        scanner.close();
+//    }
+//
     }
 }
