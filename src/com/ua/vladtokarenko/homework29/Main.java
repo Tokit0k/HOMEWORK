ea9e69f8429617a0a24683cdce93fd29bda68196
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-04-27 18:06:08 +0300

    done

---
+++ src/com/ua/vladtokarenko/homework29/Main.java
@@ -0,0 +1,52 @@
+package com.ua.vladtokarenko.homework29;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchFieldException {
+        Field[] fields = Reflection.class.getDeclaredFields();
+
+        List<Field> publicFields = new ArrayList<>();
+        List<Field> privateFields = new ArrayList<>();
+        List<Field> protectedFields = new ArrayList<>();
+        List<Field> otherFields = new ArrayList<>();
+
+        for (Field field : fields) {
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                publicFields.add(field);
+            } else if (Modifier.isPrivate(modifiers)) {
+                privateFields.add(field);
+            } else if (Modifier.isProtected(modifiers)) {
+                protectedFields.add(field);
+            } else {
+                otherFields.add(field);
+            }
+        }
+
+        System.out.println("Public fields:");
+        for (Field field : publicFields) {
+            System.out.println(field.getName());
+        }
+
+        System.out.println("Private fields:");
+        for (Field field : privateFields) {
+            System.out.println(field.getName());
+        }
+
+        System.out.println("Protected fields:");
+        for (Field field : protectedFields) {
+            System.out.println(field.getName());
+        }
+
+        System.out.println("Other fields:");
+        for (Field field : otherFields) {
+            System.out.println(field.getName());
+        }
+    }
+}
+
+
---
+++ src/com/ua/vladtokarenko/homework29/Reflection.java
@@ -0,0 +1,18 @@
+package com.ua.vladtokarenko.homework29;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reflection {
+    public int publicField1;
+    public String publicField2;
+    private boolean privateField1;
+    private int privateField2;
+    private Object privateField3;
+    protected double protectedField1;
+    protected String protectedField2;
+    static int staticField;
+    transient int transientField;
+}
+
