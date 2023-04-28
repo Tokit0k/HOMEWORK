c5bd9f05621518a1774ac0b5444ed35c99efa6b8
Vladyslav <122303952+Tokit0k@users.noreply.github.com>
2023-04-28 03:27:52 +0300

    done

--- src/com/ua/vladtokarenko/homework30/Main.java
+++ src/com/ua/vladtokarenko/homework30/Main.java
@@ -1,10 +1,12 @@
 package com.ua.vladtokarenko.homework30;
 public class Main {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new PrintNumbers("Thread 1", 10));
-        Thread thread2 = new Thread(new PrintNumbers("Thread 2", 10));
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new PrintNumbers("Thread 1", 6));
+        Thread thread2 = new Thread(new PrintNumbers("Thread 2", 7));
+        Thread thread3 = new Thread(new PrintNumbers("Thread 3", 8));
 
-        thread1.start();
         thread2.start();
+        thread1.start();
+        thread3.start();
     }
 }
